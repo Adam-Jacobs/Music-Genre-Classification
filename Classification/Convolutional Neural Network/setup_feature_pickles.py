d9068812c303bfe10f3907580525493f4bc0c344
Adam Jacobs <adam.jac@hotmail.co.uk>
2019-02-25 15:48:02 +0000

    Add concurrency to CNN feature setup

--- Classification/Convolutional Neural Network/setup_feature_pickles.py
+++ Classification/Convolutional Neural Network/setup_feature_pickles.py
@@ -16,30 +16,41 @@ image_sizeY = image_endY - image_startY
 image_size = image_sizeX * image_sizeY
 
 
+def load_spectrogram(path):
+    try:
+        # Read the image into memory
+        data_point = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+        # Crop to include only the spectrogram (thus removing the axis and labelling)
+        data_point = data_point[image_startY:image_endY, image_startX:image_endX]
+
+        # Scale the image down for better memory management
+        data_point = cv2.resize(data_point, (0, 0), fx=0.3, fy=0.3)  # ideally temporary until I figure a better way
+
+        return [os.path.basename(path).split('.')[0], data_point]
+    except Exception as e:
+        print("Exception for reading image", e, path)
+
+    return None
+
+
 def load_spectrograms():
-    spectrogramsPath = "..\\..\\..\\..\\..\\..\\FYP_Data\\spectrogram_images"
+    spectrograms_dir = "..\\..\\..\\..\\..\\..\\FYP_Data\\spectrogram_images"
+    spectrogram_file_paths = os.listdir(spectrograms_dir)
+    spectrogram_file_paths = [os.path.join(spectrograms_dir, x) for x in spectrogram_file_paths]
+    spectrogram_file_paths = spectrogram_file_paths
+
     data = []
-    spectrogram_file_paths = os.listdir(spectrogramsPath)
-    for img in tqdm.tqdm(spectrogram_file_paths):
-        try:
-            # Read the image into memory
-            data_point = cv2.imread(os.path.join(spectrogramsPath, img), cv2.IMREAD_GRAYSCALE)
 
-            # Crop to include only the spectrogram (thus removing the axis and labelling)
-            data_point = data_point[image_startY:image_endY, image_startX:image_endX]
-
-            # Scale the image down for better memory management
-            data_point = cv2.resize(data_point, (0, 0), fx=0.3, fy=0.3)  # ideally temporary until I figure a better way
-
-            # Append the features and the track number to the list
-            data.append([data_point, img.split('.')[0]])
-        except Exception as e:
-            print("Exception for reading image", e, os.path.join(spectrogramsPath, img))
+    with Pool(os.cpu_count() - 1) as pool:
+        data.extend(list(tqdm.tqdm(pool.imap(load_spectrogram, spectrogram_file_paths),
+                                   total=len(spectrogram_file_paths))))
 
     return data
 
 
-spectrograms = []
+spectrograms_ids = []
+spectrograms_features = []
 training_labels = []
 validation_labels = []
 test_labels = []
@@ -47,47 +58,48 @@ test_labels = []
 
 def setup_data():
     print('Loading spectrograms...')
-    spectrograms.extend(load_spectrograms())
+    spectrograms = load_spectrograms()
+
+    pickle_out = open("feature_pickles\\spectrogram_features_unsorted.pickle", "wb")
+    pickle.dump(spectrograms, pickle_out)
+    pickle_out.close()
+
+    spectrograms_ids.extend([x[0] for x in spectrograms])
+    spectrograms_features.extend([x[1] for x in spectrograms])
 
     print('Loading subset labels...')
-    pickle_in = open("..\\..\\fma_metadata_applied\\pickles\\training_labels.pickle", "rb")
+    pickle_in = open("..\\..\\dataset labels\\pickles\\training_labels.pickle", "rb")
     training_labels.extend(pickle.load(pickle_in))
 
-    pickle_in = open("..\\..\\fma_metadata_applied\\pickles\\validation_labels.pickle", "rb")
+    pickle_in = open("..\\..\\dataset labels\\pickles\\validation_labels.pickle", "rb")
     validation_labels.extend(pickle.load(pickle_in))
 
-    pickle_in = open("..\\..\\fma_metadata_applied\\pickles\\testing_labels.pickle", "rb")
+    pickle_in = open("..\\..\\dataset labels\\pickles\\testing_labels.pickle", "rb")
     test_labels.extend(pickle.load(pickle_in))
-
-
-def find_spectrogram_features(track_id):
-    for index, item in enumerate(spectrograms):
-        if item[1] == track_id:
-            return item[0]
 
 
 if __name__ == "__main__":
     print('Setting up data...')
     setup_data()
 
+    print('Organising features into correct subsets...')
+    pool = Pool(os.cpu_count() - 1)
+
     training_features = []
     validation_features = []
     test_features = []
 
-    print('Organising features into corect subsets...')
-    for track_id, labels in training_labels:
-        training_features.append(find_spectrogram_features(str(track_id)))
+    print('Organising training set...')
+    for track_id, labels in tqdm.tqdm(training_labels):
+        training_features.append(spectrograms_features[spectrograms_ids.index('{:06d}'.format(int(track_id)))])
 
-    for track_id, labels in validation_labels:
-        validation_features.append(find_spectrogram_features(str(track_id)))
+    print('Organising validation set...')
+    for track_id, labels in tqdm.tqdm(validation_labels):
+        validation_features.append(spectrograms_features[spectrograms_ids.index('{:06d}'.format(int(track_id)))])
 
-    for track_id, labels in test_labels:
-        test_features.append(find_spectrogram_features(str(track_id)))
-
-    # Reshape the features ready for use with CNN
-    # training_features = np.array(training_features).reshape(-1, image_sizeX, image_sizeY, 1)
-    # validation_features = np.array(validation_features).reshape(-1, image_sizeX, image_sizeY, 1)
-    # test_features = np.array(test_features).reshape(-1, image_sizeX, image_sizeY, 1)
+    print('Organising testing set...')
+    for track_id, labels in tqdm.tqdm(test_labels):
+        test_features.append(spectrograms_features[spectrograms_ids.index('{:06d}'.format(int(track_id)))])
 
     # Save the data
     print('Saving data to pickles...')
@@ -110,4 +122,10 @@ if __name__ == "__main__":
     allfeatures.extend(validation_features)
     allfeatures.extend(test_features)
 
-    np.savetxt("data\\features.csv", allfeatures, delimiter=",", fmt="%s")
+    np.savetxt("features.csv", [x[1] if x is not None else None for x in allfeatures], delimiter=",", fmt="%s")
+
+    # Reshape the features ready for use with CNN
+    # training_features = np.array(training_features).reshape(-1, image_sizeX, image_sizeY, 1)
+    # validation_features = np.array(validation_features).reshape(-1, image_sizeX, image_sizeY, 1)
+    # test_features = np.array(test_features).reshape(-1, image_sizeX, image_sizeY, 1)
+
