8a84131f4202a8556d639856409ea56822c3cd2b
Adam Jacobs <adam.jac@hotmail.co.uk>
2019-03-01 10:44:00 +0000

    Make CNN train

--- Classification/Convolutional Neural Network/CNN.py
+++ Classification/Convolutional Neural Network/CNN.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -7,9 +8,6 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D
 
 import pickle
 
-#pickle_in = open("genres.pickle", "rb")
-#genres = pickle.load(pickle_in)
-
 pickle_in = open("feature_pickles\\training_features.pickle", "rb")
 training_features = pickle.load(pickle_in)
 
@@ -17,13 +15,14 @@ pickle_in = open("..\\..\\dataset labels\\pickles\\training_labels.pickle", "rb"
 training_labels = pickle.load(pickle_in)
 
 # X = X/255.0 # is normalisation needed?
-print(training_labels[:10])
 model = Sequential()
 
+# Input layer
 model.add(Conv2D(256, (3, 3), input_shape=training_features.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+# Hidden Layer
 model.add(Conv2D(256, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -32,6 +31,7 @@ model.add(Flatten())  # converts 3D feature maps to 1D vectors
 
 model.add(Dense(64))
 
+# Output layer
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
@@ -39,4 +39,4 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(training_features, training_labels, batch_size=32, epochs=3, validation_split=0.3)
+model.fit(training_features, np.array([x[1] for x in training_labels]), batch_size=32, epochs=3, validation_split=0.3)
--- Classification/Convolutional Neural Network/setup_feature_pickles.py
+++ Classification/Convolutional Neural Network/setup_feature_pickles.py
@@ -112,21 +112,21 @@ if __name__ == "__main__":
         test_features.append(spectrograms_features[spectrograms_ids.index('{:06d}'.format(int(track_id)))])
 
     # csv
-    allfeatures = []
-    allfeatures.extend(training_features)
-    allfeatures.extend(validation_features)
-    allfeatures.extend(test_features)
+    # allfeatures = []
+    # allfeatures.extend(training_features)
+    # allfeatures.extend(validation_features)
+    # allfeatures.extend(test_features)
 
-    np.savetxt("feature_pickles\\features.csv", [x[1] if x is not None else None for x in allfeatures], delimiter=",", fmt="%s")
+    # np.savetxt("feature_pickles\\features.csv", [x[1] if x is not None else None for x in allfeatures],
+    #            delimiter=",", fmt="%s")
 
     # Reshape the features ready for use with CNN
-    training_features = np.array(training_features).reshape(-1, image_scaled_sizeX, image_scaled_sizeY, 1)
-    validation_features = np.array(validation_features).reshape(-1, image_scaled_sizeX, image_scaled_sizeY, 1)
-    test_features = np.array(test_features).reshape(-1, image_scaled_sizeX, image_scaled_sizeY, 1)
+    training_features = np.array(training_features).reshape(-1, image_scaled_sizeY, image_scaled_sizeX, 1)
+    validation_features = np.array(validation_features).reshape(-1, image_scaled_sizeY, image_scaled_sizeX, 1)
+    test_features = np.array(test_features).reshape(-1, image_scaled_sizeY, image_scaled_sizeX, 1)
 
     # Save the data
     print('Saving data to pickles...')
-
     pickle_out = open("feature_pickles\\training_features.pickle", "wb")
     pickle.dump(training_features, pickle_out)
     pickle_out.close()
