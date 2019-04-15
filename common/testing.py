fff4e521bd19470f45072244df0cd0276e516f63
Adam Jacobs <adam.jac@hotmail.co.uk>
2019-04-15 14:07:24 +0100

    Add unit tests to data_manipulation.py

--- common/testing.py
+++ common/testing.py
@@ -1,12 +1,51 @@
+import unittest
 import data_manipulation as dm
 import numpy as np
 
 
-features = [[1.0, 12.0],
-            [2.0, 27.0],
-            [3.0, 34.0]]
-correct_normalised_values = [[0.0, 0.5, 1.0],
-                             [0.0, 0.6818, 1.0]]
+class TestDataManipulation(unittest.TestCase):
 
-normalised_values = dm.normalise_features(features)
-print(np.array(normalised_values))
+    @classmethod
+    def setUpClass(c):
+        pass
+
+    @classmethod
+    def tearDownClass(c):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_min_max_normalise(self):
+        # Setup
+        features = [12.0, 27.0, 34.0]
+        correct_normalised_values = [0.0, 0.6818181818181818, 1.0]
+
+        # Execute
+        normalised_values = dm.min_max_normalise(features)
+
+        # Test
+        self.assertEqual(normalised_values, correct_normalised_values)
+
+    def test_normalise_features(self):
+        # Setup
+        features = [[1.0, 12.0],
+                    [2.0, 27.0],
+                    [3.0, 34.0]]
+        correct_normalised_values = [[0.0, 0.0],
+                                     [0.5, 0.6818181818181818],
+                                     [1.0, 1.0]]
+
+        # Execute
+        normalised_values = dm.normalise_features(features)
+
+        # Test
+        self.assertEqual(normalised_values, correct_normalised_values)
+
+
+'''Allows unit tests to be run from cmd the same way as normal .py file'''
+if __name__ == '__main__':
+    unittest.main()
